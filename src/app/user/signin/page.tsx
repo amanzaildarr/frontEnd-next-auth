b4adb1d9948c05c69583aad9d9acd9c6dfe0d8f8
Amandeep Singh <115974363+amanzaildar01@users.noreply.github.com>
2023-08-11 08:48:24 +0530

    middleware testing

--- src/app/user/signin/page.tsx
+++ src/app/user/signin/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 import apiService from '@/api/api.service';
 import ApiRoutes from '@/api/enums/api.routes.enums';
-import { AxiosResponse } from 'axios';
-import Link from 'next/link';
+import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
@@ -14,12 +13,20 @@ const LoginPage: React.FC = () => {
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         apiService.post<{ token: string }>(ApiRoutes.Login, { email: username, password })
-        .then(response => {
-            console.log('ress::::', response)
-        })
-        .catch(err=>{
-            console.log('err::',err)
-        })
+            .then(response => {
+                Cookies.set('JWT_TOKEN', response.data.token, {
+                    expires: 30, // Cookie expiration time (30 days)
+                    path: '/',
+                    secure: process.env.NODE_ENV === 'production', // Set secure flag in production
+                    sameSite: 'strict', // Set SameSite attribute
+                  });
+
+                console.log('Token saved in cookie:', response.data.token);
+                return Router.push('/')
+            })
+            .catch(err => {
+                console.log('err::', err)
+            })
     };
 
     return (
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: Request){
-    console.log('middlware');
-    console.log('requset',request.method);
-    console.log('requset:ur;:::',request.url);
-    return NextResponse.next()
+export function middleware(request: NextRequest) {
+    console.log('middleware');
+    // request.headers.delete('cookie')
+    // request.headers.delete('cookie')
+    const token = request.headers.get('cookie');
+    const test = request.headers.append('cookie', "")
+    console.log('token', token, test);
+    if (!token) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/user/login'
+        return NextResponse.rewrite(url)
+
+    }
+
+    return NextResponse.next();
 }
-// export const config = {
-//     matcher:['/user/sigin']
-// }
